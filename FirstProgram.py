7bd2a8f0fc036860566e5d1350f8ce33f118e3c4
AneesAlam1 <106650151+AneesAlam1@users.noreply.github.com>
2022-06-03 10:56:02 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,7 +1,11 @@
 print("We have started the project now all the developers can include their work time to time")
 print("When you do some modification in file please mention your name as company policy")
 lst=[1,2,3,4,5,6,7,8,9,10]
-for i in lst
-print(i)
+for i in lst:
+	print(i)
 
- 
+print("I have done some modification")
+print("sum ",sum(lst))
+print("Length ",len(lst))
+lst.sort()
+print(lst)
