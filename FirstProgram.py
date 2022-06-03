fa217387ca06fa9d6adcac57fbb2a85c3f1093da
AneesAlam1 <106650151+AneesAlam1@users.noreply.github.com>
2022-06-03 10:43:08 +0530

    Create FirstProgram.py
    Project started

---
+++ FirstProgram.py
@@ -0,0 +1,7 @@
+print("We have started the project now all the developers can include their work time to time")
+print("When you do some modification in file please mention your name as company policy")
+lst=[1,2,3,4,5]
+for i in lst
+print(i)
+
+ 
