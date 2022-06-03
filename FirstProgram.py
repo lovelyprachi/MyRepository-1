33e31477414fae7c05b5be6fc6ab0204dc0fe7a7
AneesAlam1 <106650151+AneesAlam1@users.noreply.github.com>
2022-06-03 10:44:31 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,6 +1,6 @@
 print("We have started the project now all the developers can include their work time to time")
 print("When you do some modification in file please mention your name as company policy")
-lst=[1,2,3,4,5]
+lst=[1,2,3,4,5,6,7,8,9,10]
 for i in lst
 print(i)
 
